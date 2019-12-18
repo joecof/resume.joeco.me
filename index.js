0c35e78521f97d9e13e9b1c5f9690cb5fb5511c7
joeco fong <joecofg@gmail.com>
2019-12-18 14:08:24 -0800

    configured the index.js file

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express');
+
+// Constants
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+// App
 const app = express();
+app.get('/', (req, res) => {
+  res.send('Hello world\n');
+});
 
-
-app.get('/', (req, res, next) => {
-  res.send("hello world");
-})
-
-app.listen(3000, () => {
-  console.log("listening on port 3000");
-})
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
