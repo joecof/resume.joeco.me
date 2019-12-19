bdafe61b42feebefe74929fae20af398ff4dfc46
joeco fong <joecofg@gmail.com>
2019-12-18 16:09:54 -0800

    change

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const express = require('express');
 
 // Constants
 const PORT = 3000;
-const HOST = '3.137.50.204';
+const HOST = '0.0.0.0';
 
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello world, testing additional changes\n');
+  res.send('Hello world\n');
 });
 
 app.listen(PORT, HOST);
