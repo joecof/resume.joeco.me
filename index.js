b72cbe222b948ccaee89b2be8c0cdd41d26c5e1a
joeco fong <joecofg@gmail.com>
2019-12-19 15:23:44 -0800

    added bodyParser

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const helmet = require('helmet');
 const path = require('path');
+const bodyParser = require('body-parser');
 
 app.use(helmet());
 app.use(bodyParser.urlencoded({
