fcea5225fd28799ba389333b321eed60870ea985
joeco fong <joecofg@gmail.com>
2019-12-18 19:52:00 -0800

    reset back to default

--- index.js
+++ index.js
@@ -1,17 +1,8 @@
 const express = require('express');
 
-// App
 const app = express();
 app.get('/', (req, res) => {
   res.send('Hello world\n');
 });
-
-app.get('/api', (req, res) => {
-  res.send("we are in api route");
-})
-
-app.get('/controller', (req, res) => {
-  res.send("testing fast development");
-})
 
 app.listen(process.env.PORT || 3000);
