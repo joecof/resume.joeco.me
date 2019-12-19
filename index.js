ba1145f1fef2c5c660f4cd2dc695d882dda392dd
joeco fong <joecofg@gmail.com>
2019-12-18 17:42:40 -0800

    commit

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
 const express = require('express');
 
-// Constants
-const PORT = 3000;
-const HOST = 172.31.33.54;
-
 // App
 const app = express();
 app.get('/', (req, res) => {
   res.send('Hello world\n');
 });
 
-app.listen(process.env.PORT || PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
+var server = app.listen(process.env.PORT || 3000);
